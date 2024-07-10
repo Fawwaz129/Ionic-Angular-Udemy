5203f6ff76e3616c1723f5499b55337509a22fd8
Fawwaz Maulana <wazwazzsrhrr@gmail.com>
2024-07-10 11:18:24 +0700

    Managing state with service

---
+++ src/app/pages/recipes/recipes.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+    id: 'r1',
+    title: 'Schinitzel',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    ingredients: ['French Fries', 'Pork meat', 'Salad']
+  },
+  {
+    id: 'r2',
+    title: 'Spaghetti',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    ingredients: ['Spaghetti', 'Meat', 'Tomato']
+  }
+];
+  constructor() { }
+
+  getAllRecepis() {
+    return [...this.recipes];
+  }
+  getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })}
+  }
+}
